379dde14b24456756da0a84e2d1359d11a67a448
STeven L1i <yue_tp_steven@apple.com>
2022-11-21 18:48:58 +0800

    def

---
+++ cppdefault/cmake-build-debug/CMakeFiles/cppdefault.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cppdefault.dir/main.cpp.o"
+  "CMakeFiles/cppdefault.dir/main.cpp.o.d"
+  "cppdefault"
+  "cppdefault.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cppdefault.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
